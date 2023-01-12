e884890a20fb89dc9a1e9c7dafdc4eaed652ea67
Edward Shanahan <edwardshanahan357@gmail.com>
2023-01-12 18:45:39 +0000

    Display addation question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = this.getAttribute("data-type");
 
-                alert(gameType)
+                runGame(gameType);
             }
         })
     }
@@ -17,9 +17,16 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown Game Type  + ${gameType}`);
+        throw `Unknown Game Type ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -38,8 +45,10 @@ function incrementWrongScore() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
